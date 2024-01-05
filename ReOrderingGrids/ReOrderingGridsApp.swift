cc4951601ae4e0890d01503851465dbab4e868a1
XJPIT <138988193+xjpit@users.noreply.github.com>
2024-01-05 09:49:19 +0800

    Initial Commit

---
+++ ReOrderingGrids/ReOrderingGridsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReOrderingGridsApp.swift
+//  ReOrderingGrids
+//
+//  Created by daryl on 5/1/24.
+//
+
+import SwiftUI
+
+@main
+struct ReOrderingGridsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
