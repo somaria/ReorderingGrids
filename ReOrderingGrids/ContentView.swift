bf69a7300402722ce00eb0ae80889f3263d1d5da
XJPIT <138988193+xjpit@users.noreply.github.com>
2024-01-12 11:28:41 +0800

    update

--- ReOrderingGrids/ContentView.swift
+++ ReOrderingGrids/ContentView.swift
@@ -6,17 +6,77 @@
 //
 
 import SwiftUI
+import WrappingHStack
+
 
 struct ContentView: View {
+    
+    @State private var colors: [String] = ["This is a", "cat", "and", "a mouse", "a dog", "the dragon"]
+    
+    @State private var draggingItem: String?
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        //        NavigationStack {
+        
+        //                let columns = Array(repeating: GridItem(spacing: 10), count: 3)
+        
+        ScrollView {
+            WrappingHStack(alignment: .leading) {
+                
+                ForEach(colors, id: \.self) { color in
+                    //                        GeometryReader {
+                    //                            let _ = $0.size
+                    
+                    Text(color)
+                        .fixedSize()
+                        .padding(.leading, 3)
+                        .padding(.trailing, 3)
+                    
+                        .font(.largeTitle)
+                        .draggable(color) {
+                            Text(color)
+                                .border(.red)
+                                .onAppear {
+                                    draggingItem = color
+                                }
+                        }
+                        .dropDestination(for: String.self) { items, location in
+                            draggingItem = nil
+                            return false
+                        } isTargeted: { status in
+                            
+                            if let draggingItem, status, draggingItem != color {
+                                
+                                if let sourceIndex = colors.firstIndex(of: draggingItem),
+                                   
+                                    let destinationIndex = colors.firstIndex(of: color) {
+                                    
+                                    withAnimation(.bouncy) {
+                                        let sourceItem = colors.remove(at: sourceIndex)
+                                        
+                                        colors.insert(sourceItem, at: destinationIndex)
+                                        
+                                    }
+                                    
+                                }
+                                
+                            }
+                            //                                    }
+                        }
+                        .padding(.leading, 6)
+                }
+                Spacer()
+            }
+            
+            
+            Button {
+            } label: {
+                Text("Submit")
+            }
+            
         }
-        .padding()
     }
+    
 }
 
 #Preview {
